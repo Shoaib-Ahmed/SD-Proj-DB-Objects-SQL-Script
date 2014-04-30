c5ad6cf5cc22283858558666a006238453c1271b
Shoaib <sahmed25@asu.edu>
2014-04-29 20:43:10 -0700

    Create Endpoints_Cretae_Table_Script.sql

---
+++ DB-Objects/Endpoints_Cretae_Table_Script.sql
@@ -0,0 +1,5 @@
+CREATE TABLE `endpoints` (
+  `user` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `endpoint` int(11) unsigned NOT NULL,
+  PRIMARY KEY (`user`)
+) ENGINE=InnoDB AUTO_INCREMENT=32 DEFAULT CHARSET=latin1$$
